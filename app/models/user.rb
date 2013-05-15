0b5067aaedb215e2540178a92ab82502381e0402
Apprentice <apprentice@devbootcamp.com>
2013-05-15 11:01:25 -0700

    Included BCrypt in UserModel

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_many :questions
   has_many :votes
   has_many :answers
@@ -11,6 +13,7 @@ class User < ActiveRecord::Base
 
    validate :valid_email
 
+   include 'BCrypt'
 
   def valid_email
     unless email =~ /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
