669c5a818d0624a13016399fffcf62d871a9064a
Angie Bui <angiebui@gmail.com>
2013-05-16 07:27:48 -0700

    added ability to update or create vote with one vote per user

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,11 @@
 class AnswersController < ApplicationController
+
+  before_filter :get_answer_id, only: [:upvote, :downvote, :create_vote]
+
+  def get_answer_id
+    @answer = Answer.find(params[:id])
+  end
+
   def create
     if current_user
       @answer = Answer.new(params[:answer])
@@ -11,23 +18,39 @@ class AnswersController < ApplicationController
   end
 
   def upvote
-    @answer = Answer.find(params[:id])
-    @vote = Vote.new
-    @vote.user_id = current_user.id
-    @vote.answer_id = @answer.id
-    @vote.upvote = true
-    @vote.save!
-    render :nothing => true
+    vote = Vote.find_by_user_id_and_answer_id(current_user.id, @answer.id)
+    # refactor this to look for the primary "composite key"
+    if vote
+      update_vote(vote, true)
+    else
+      create_vote(true)
+    end
   end
 
   def downvote
-    @answer = Answer.find(params[:id])
+   vote = Vote.find_by_user_id_and_answer_id(current_user.id, @answer.id)
+    if vote
+      update_vote(vote, false)
+    else
+      create_vote(false)
+    end
+  end
+
+  private
+  def update_vote(vote, arg)
+   vote.upvote = arg
+   vote.save
+   flash[:notice] = "Your vote has been updated!"
+   # notice is currently not showing up
+  end
+
+  def create_vote(arg)
     @vote = Vote.new
     @vote.user_id = current_user.id
     @vote.answer_id = @answer.id
-    @vote.upvote = false
+    @vote.upvote = arg
     @vote.save!
     render :nothing => true
+    flash[:notice] = "Your vote has been created!"
   end
-
 end
