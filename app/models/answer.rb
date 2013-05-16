23238935c731c85b69144257fdf11bf104f35499
Apprentice <apprentice@devbootcamp.com>
2013-05-16 11:22:26 -0700

    Styled answering, votes, added time_ago to answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,4 +6,10 @@ class Answer < ActiveRecord::Base
   validates_presence_of :body
 
   attr_accessible :body, :question_id, :user_id
+
+  def time_ago 
+    ((Time.now - self.created_at) / 1.hour).round
+    
+  end
+
 end
