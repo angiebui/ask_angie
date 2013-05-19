7aa99d834da3d6596496ec7f2f0f18a6f4ce2f6e
Apprentice <apprentice@devbootcamp.com>
2013-05-19 15:51:11 -0700

    added delete and edit buttons for questions on profile page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -19,32 +19,44 @@ class QuestionsController < ApplicationController
 
   def create
    if current_user
-      @topic = Topic.find(params[:topic_id])
-      @question = current_user.questions.build(params[:question])
-      @question.topic_id = @topic.id
-      @answer = Answer.new
-      
-      if @question.save
-        redirect_to @question
-      else
-        flash[:notice] = "Not a valid question. Please try again."
-        redirect_to new_question_path
-      end
+    @topic = Topic.find(params[:topic_id])
+    @question = current_user.questions.build(params[:question])
+    @question.topic_id = @topic.id
+    @answer = Answer.new
+
+    if @question.save
+      redirect_to @question
     else
-      flash[:notice] = "You must be logged in to create a question."
-      redirect_to login_users_path
+      flash[:notice] = "Not a valid question. Please try again."
+      redirect_to new_question_path
     end
+  else
+    flash[:notice] = "You must be logged in to create a question."
+    redirect_to login_users_path
   end
+end
 
-  def edit
-    @question = Question.find(params[:id])
-    @topics = Topic.all
-  end
+def edit
+  @question = Question.find(params[:id])
+  @topics = Topic.all
+end
 
-  def search
-    @questions_by_answer_count = Question.search params[:search]
-    @search = params[:search]
-  end
+def update
+  @question = Question.find(params[:id])
+  @question.update_attributes(params[:question])
+  redirect_to user_path(current_user.id)
+end
+
+def destroy
+  @question = Question.find(params[:id])
+  @question.destroy
+  redirect_to user_path(current_user.id)
+end
+
+def search
+  @questions_by_answer_count = Question.search params[:search]
+  @search = params[:search]
+end
 
 end
 
